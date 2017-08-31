235562cf26da02769ec9c2c3edcbdb82606c014e
skim-chee <kim.seean@gmail.com>
2017-08-30 20:07:10 -0400

    ADDED: Lazy loading images

--- assets/js/index.js
+++ assets/js/index.js
@@ -54,3 +54,12 @@ $(document).ready(function(){
         }
     });
 });
+
+// Lazy load
+
+[].forEach.call(document.querySelectorAll('img[data-src]'),    function(img) {
+  img.setAttribute('src', img.getAttribute('data-src'));
+  img.onload = function() {
+    img.removeAttribute('data-src');
+  };
+});
